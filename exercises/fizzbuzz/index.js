7258fd202b72940274d0a35d8451142943da4997
sooin.c <sooinc90@gmail.com>
2020-05-01 17:09:59 -0400

    fizzbuzz solutions added

--- exercises/fizzbuzz/index.js
+++ exercises/fizzbuzz/index.js
@@ -12,6 +12,37 @@
 //   4
 //   buzz
 
-function fizzBuzz(n) {}
+// function fizzBuzz(n) {
+//   for (let i = 1; i <= n; i++) {
+//     if (i % 3 === 0 && i % 5 === 0) {
+//       console.log('fizzbuzz');
+//       continue;
+//     }
+//     if (i % 3 === 0) {
+//       console.log('fizz');
+//       continue;
+//     }
+//     if (i % 5 === 0) {
+//       console.log('buzz');
+//       continue;
+//     }
+//     console.log(i);
+//   }
+// }
+
+//refactored above solution
+function fizzBuzz(n) {
+  for (let i = 1; i <= n; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log('fizzbuzz');
+    } else if (i % 3 === 0) {
+      console.log('fizz');
+    } else if (i % 5 === 0) {
+      console.log('buzz');
+    } else {
+      console.log(i);
+    }
+  }
+}
 
 module.exports = fizzBuzz;
