dc0443e297f045d875b87dd272a91ae612ef77b9
sooin.c <sooinc90@gmail.com>
2020-05-03 14:18:46 -0400

    solutions for anagrams and chunks

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,75 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+  let wordA = stringA.replace(/[^\w]/g, '').toLowerCase();
+  let wordB = stringB.replace(/[^\w]/g, '').toLowerCase();
+
+  if (wordA.length !== wordB.length) return false;
+
+  let objA = {};
+  let objB = {};
+  for (let i = 0; i < wordA.length; i++) {
+    let charA = wordA[i];
+    let charB = wordB[i];
+    if (!objA[charA]) {
+      objA[charA] = 1;
+    } else {
+      objA[charA] += 1;
+    }
+    if (!objB[charB]) {
+      objB[charB] = 1;
+    } else {
+      objB[charB] += 1;
+    }
+  }
+
+  for (let key in objA) {
+    if (objA[key] !== objB[key]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+// //using helper function
+// function anagrams(stringA, stringB) {
+//   let mapOfA = mapString(stringA);
+//   let mapOfB = mapString(stringB);
+
+//   if (Object.keys(mapOfA).length !== Object.keys(mapOfB).length) return false;
+
+//   for (let key in mapOfA) {
+//     if (mapOfA[key] !== mapOfB[key]) {
+//       return false;
+//     }
+//   }
+//   return true;
+// }
+
+// function mapString(string) {
+//   let mapA = {};
+//   for (let char of string.replace(/[^\w]/g, '').toLowerCase()) {
+//     mapA[char] = mapA[char] + 1 || 1;
+//   }
+//   return mapA;
+// }
+
+//use sort! then we can just compare str to str vs obj to obj
+// function anagrams(stringA, stringB) {
+//   let cleanStrA = cleanStr(stringA);
+//   let cleanStrB = cleanStr(stringB);
+
+//   return cleanStrA === cleanStrB;
+// }
+
+// function cleanStr(string) {
+//   return string
+//     .replace(/w[^\w]/g, '')
+//     .toLowerCase()
+//     .split('')
+//     .sort()
+//     .join('');
+// }
 
 module.exports = anagrams;
--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,54 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+// iterable solution
+function chunk(array, size) {
+  let finalArr = [];
+
+  if (size >= array.length) {
+    finalArr.push(array);
+  } else {
+    let subArr = [];
+    for (let i = 0; i < array.length; i++) {
+      subArr.push(array[i]);
+      if (subArr.length === size) {
+        finalArr.push(subArr);
+        subArr = [];
+      }
+    }
+    if (subArr.length > 1) {
+      finalArr.push(subArr);
+    }
+  }
+  return finalArr;
+}
+
+// //another iterative way
+// function chunk(array, size) {
+//   let finalArr = [];
+
+//   for (let i = 0; i < array.length; i++) {
+//     let lastEl = finalArr[finalArr.length - 1];
+//     let originalEl = array[i];
+//     if (!lastEl || lastEl.length === size) {
+//       finalArr.push([originalEl]);
+//     } else {
+//       lastEl.push(originalEl);
+//     }
+//   }
+//   return finalArr;
+// }
+
+// //slice + while loop solution
+// function chunk(array, size) {
+//   let finalArr = [];
+//   let i = 0;
+
+//   while (i < array.length) {
+//     finalArr.push(array.slice(i, i + size));
+//     i += size;
+//   }
+//   return finalArr;
+// }
 
 module.exports = chunk;
