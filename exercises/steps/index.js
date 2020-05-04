ae8e5d535f21823c7801de17d759a50a46881cd5
sooin.c <sooinc90@gmail.com>
2020-05-04 13:40:44 -0400

    steps solutions added

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,39 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+function steps(n) {
+  for (let row = 0; row < n; row++) {
+    let stair = '';
+    for (let column = 0; column < n; column++) {
+      if (column <= row) {
+        stair += '#';
+      } else {
+        stair += ' ';
+      }
+    }
+    console.log(stair);
+  }
+}
+
+// function steps(n, row = 0, stair = '') {
+//   if (row === n) {
+//     //row reached the number of levels given
+//     return;
+//   }
+
+//   if (stair.length === n) {
+//     //column reached the end so
+//     console.log(stair); //at which point we can console
+//     steps(n, row + 1); //and start the building of the column on the next row (so the stair is '')
+//     return;
+//   }
+
+//   if (stair.length <= row) {
+//     stair += '#';
+//   } else {
+//     stair += ' ';
+//   }
+//   steps(n, row, stair); //only happens when row and column has not met its end
+// }
 
 module.exports = steps;
