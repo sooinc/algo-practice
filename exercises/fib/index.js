5714f418e1e638aa470980b3a2e0512c6ccbddb3
sooin.c <sooinc90@gmail.com>
2020-05-05 21:47:34 -0400

    fib solutions added

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,80 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+function memoize(fn) {
+  const cache = {};
+  return function (args) {
+    //if we don't know how many arg it'll take use --> ...args
+    if (cache[args]) {
+      return cache[args];
+    }
+    //if we took ...args, we would do --> cache[args] = fn.apply(this, args);
+    cache[args] = fn(args);
+    return cache[args];
+  };
+}
+
+function slowFib(n) {
+  if (n < 2) {
+    return n;
+  } else {
+    return fib(n - 1) + fib(n - 2);
+  }
+}
+
+let fib = memoize(slowFib);
+
+console.log(fib(15));
 
 module.exports = fib;
+
+// function fib(n) {
+//   if (n === 0) {
+//     return 0;
+//   }
+//   if (n === 1) {
+//     return 1;
+//   }
+//   if (n > 1) {
+//     return fib(n - 1) + fib(n - 2);
+//   }
+// }
+
+// function fib(n, mem = { 0: 0, 1: 1, 2: 1 }) {
+//   if (mem[n]) {
+//     return mem[n];
+//   } else {
+//     mem[n] = fib(n - 1, mem) + fib(n - 2, mem);
+//     return mem[n];
+//   }
+// }
+
+//ITERATIVE SOLUTIONS
+
+// //O(n) time/ O(n) space
+// function fib(n) {
+//   let result = [0, 1];
+
+//   for (let i = 2; i <= n; i++) {
+//     let a = result[i - 1];
+//     let b = result[i - 2];
+
+//     result.push(a + b);
+//   }
+//   return result[n];
+// }
+
+// //O(n) time/ O(1) space
+// function fib(n) {
+//   if (n === 0) return 0;
+//   let lastTwo = [0, 1];
+
+//   // THIS IS THE KEY!!! STARTING THE COUNT AT 2. (or 3 if the fib series starts at 1 not 0)
+//   for (let i = 2; i <= n; i++) {
+//     let current = lastTwo[0];
+//     lastTwo[0] = lastTwo[1];
+//     lastTwo[1] = lastTwo[1] + current;
+//   }
+
+//   return lastTwo[1];
+// }
--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -18,3 +18,47 @@
 function matrix(n) {}
 
 module.exports = matrix;
+
+// function matrix(n) {
+//     let result = [];
+//     for (let i = 0; i < n; i++) {
+//       result.push([]);
+//     }
+
+//     let counter = 1;
+//     let startRow = 0;
+//     let endRow = n - 1;
+//     let startCol = 0;
+//     let endCol = n - 1;
+
+//     while (startCol <= endCol && startRow <= endRow) {
+//       //first row
+//       for (let i = startCol; i <= endCol; i++) {
+//         result[startRow][i] = counter;
+//         counter += 1;
+//       }
+//       startRow += 1;
+
+//       //last column
+//       for (let i = startRow; i <= endRow; i++) {
+//         result[i][endCol] = counter;
+//         counter += 1;
+//       }
+//       endCol -= 1;
+
+//       //last row
+//       for (let i = endCol; i >= startCol; i--) {
+//         result[endRow][i] = counter;
+//         counter += 1;
+//       }
+//       endRow -= 1;
+
+//       //last column
+//       for (let i = endRow; i >= startRow; i--) {
+//         result[i][startCol] = counter;
+//         counter += 1;
+//       }
+//       startCol += 1;
+//     }
+//     return result;
+//   }
