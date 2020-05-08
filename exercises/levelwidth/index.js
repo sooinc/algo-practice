7b8f5139b08227d9a540df843ea57edded1e9d72
sooin.c <sooinc90@gmail.com>
2020-05-08 16:49:10 -0400

    levelwidth binary tree solution added

--- exercises/levelwidth/index.js
+++ exercises/levelwidth/index.js
@@ -11,6 +11,22 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+function levelWidth(root) {
+  let counter = [0];
+  let allNodes = [root, 'stop'];
+
+  while (allNodes.length > 1) {
+    let remove = allNodes.shift();
+    if (remove === 'stop') {
+      allNodes.push(remove);
+      counter.push(0);
+      continue;
+    }
+    allNodes.push(...remove.children);
+    counter[counter.length - 1]++;
+  }
+
+  return counter;
+}
 
 module.exports = levelWidth;
