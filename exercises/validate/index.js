b8df61117ceda545204f1eacb18f8b223df8b016
sooin.c <sooinc90@gmail.com>
2020-05-08 20:48:51 -0400

    validate bst added

--- exercises/validate/index.js
+++ exercises/validate/index.js
@@ -5,6 +5,30 @@
 // every node's right hand child is greater than
 // the parent
 
-function validate(node, min = null, max = null) {}
+//move to the right - we update min value
+//move to the left - we update max value
+function validate(node, min = null, max = null) {
+  if (max !== null && node.data > max) {
+    return false;
+  }
+
+  if (min !== null && node.data < min) {
+    return false;
+  }
+
+  if (node.left && !validate(node.left, min, node.data)) {
+    //if the validate() recursive call returns false then we know it's not a valid BST
+    //and we enter into this if statement to return false
+    return false;
+  }
+
+  if (node.right && !validate(node.right, node.data, max)) {
+    //if the validate() recursive call returns false then we know it's not a valid BST
+    //and we enter into this if statement to return false
+    return false;
+  }
+
+  return true;
+}
 
 module.exports = validate;
